52cde865c0524e9be3b03df9f6add8c6cce44bfe
HoCheon95 <dcr584@gmail.com>
2026-04-03 12:52:01 +0900

    day_15

--- react-project-ts/src/main.tsx
+++ react-project-ts/src/main.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+/**
+ * 1. 진입점 파일은 애플리케이션이 브라우저에서 실행되는 최초의 시작 지점
+ * 2. React 자체 기능이 아니라, 리액트를 웹 페이지에 연결하는 역할을 담당한다<div className="">
+ */
 createRoot(document.getElementById('root')!).render(
     <App />
 )
